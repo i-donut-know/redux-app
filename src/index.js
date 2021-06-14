dbf40ddebc1528e7fd68c4ad34419e285a9df7da
Kseniya <mylittleuniverse@list.ru>
2021-06-14 23:08:30 +0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const reducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'INC':
+            return state + 1;
+        case 'DEC':
+            return state - 1;
+        default: 
+            return state;
+    }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const store = createStore(reducer);
+
+document.getElementById('inc').addEventListener('click', () => {
+    store.dispatch({type: 'INC'});
+});
+
+document.getElementById('dec').addEventListener('click', () => {
+    store.dispatch({type: 'DEC'});
+});
+
+const update = () => {
+    document.getElementById('counter').textContent = store.getState();
+}
+
+store.subscribe(update);
+
+// store.dispatch({type: 'INC'});
+// store.dispatch({type: 'INC'});
+// store.dispatch({type: 'INC'});
