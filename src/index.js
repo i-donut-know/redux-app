fa08c1ec887d7f7e736006fe434fee50802c19ad
Kseniya <mylittleuniverse@list.ru>
2021-06-17 18:43:14 +0700

    Соединяем React и Redux

--- src/index.js
+++ src/index.js
@@ -1,38 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
-
-const reducer = (state = 0, action) => {
-    switch (action.type) {
-        case 'INC':
-            return state + 1;
-        case 'DEC':
-            return state - 1;
-        case 'RES':
-            return 0;
-        default: 
-            return state;
-    }
-}
-
-const inc = () => ({type: 'INC'});
-const dec = () => ({type: 'DEC'});
-const reset = () => ({type: 'RES'});
+import {Provider} from 'react-redux';
+import reducer from './reducer';
+import App from './components/app';
 
 const store = createStore(reducer);
 
-document.getElementById('inc').addEventListener('click', () => {
-    store.dispatch(inc());
-});
-
-document.getElementById('dec').addEventListener('click', () => {
-    store.dispatch(dec());
-});
-
-document.getElementById('reset').addEventListener('click', () => {
-  store.dispatch(reset());
-});
-
-const update = () => {
-    document.getElementById('counter').textContent = store.getState();
-}
-
-store.subscribe(update);
+ReactDOM.render(
+    <Provider store={store}>
+            <App/>
+    </Provider>   
+    , document.getElementById("root"));
